111f9f1445b28754ef9a82b8cd37242a1b017830
Vaishnavi <vaishupatil6324@gmail.com>
2025-01-17 15:50:52 +0530

    Commit

--- FrontEnd/src/components/login/LoginForm.js
+++ FrontEnd/src/components/login/LoginForm.js
@@ -29,7 +29,7 @@ const LoginForm = () => {
     setIsLoading(true); // Show loading state
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://backend-pi-lac.vercel.app/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- FrontEnd/src/components/signup/SignupForm.js
+++ FrontEnd/src/components/signup/SignupForm.js
@@ -52,7 +52,7 @@ const SignupForm = () => {
     e.preventDefault();
     if (validateForm()) {
       try {
-        const response = await fetch("http://localhost:5000/api/signup", {
+        const response = await fetch("https://backend-pi-lac.vercel.app/api/signup", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
