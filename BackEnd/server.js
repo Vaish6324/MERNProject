33eee9eda221289800df1e09dca0175e8cb0d1e0
Vaishnavi <vaishupatil6324@gmail.com>
2025-01-20 11:57:48 +0530

    Commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -78,5 +78,6 @@ app.post("/api/login", async (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
